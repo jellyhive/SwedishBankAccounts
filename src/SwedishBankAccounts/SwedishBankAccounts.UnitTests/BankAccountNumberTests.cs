e1ad8cca1b759c47744c44fdb15f500904a54c46
V-Nilsson <89032509+V-Nilsson@users.noreply.github.com>
2023-05-29 11:27:27 +0200

    Added more testcases

--- src/SwedishBankAccounts/SwedishBankAccounts.UnitTests/BankAccountNumberTests.cs
+++ src/SwedishBankAccounts/SwedishBankAccounts.UnitTests/BankAccountNumberTests.cs
@@ -6,6 +6,10 @@ public class BankAccountNumberTests : UnitTests
     [InlineData("9071, 417 23 83", "Multitude Bank")]
     [InlineData("6683764450808", "Handelsbanken")]
     [InlineData("8424-4,983 189 224-6", "Swedbank")]
+    [InlineData("8351-9,392 242 224-5", "Swedbank")]
+    [InlineData("8129-9,043 386 711-6", "Swedbank")]
+    [InlineData("3300 000620-5124", "Nordea")]
+    [InlineData("97891111113", "Klarna Bank")]
     public void AccountNumber_TryParse_ShouldValidate(string accountNumber, string bankName)
     {
         BankAccountNumber.TryParse(accountNumber, out var bankAccountNumber)
