0d75f65c0d86f11202310e4cff04126f73b6a62b
Jonas Wikström <jonas@jonaswikstrom.se>
2023-05-29 10:03:44 +0200

    Unit tests, first draft

--- src/SwedishBankAccounts/SwedishBankAccounts.UnitTests/BankAccountNumberTests.cs
+++ src/SwedishBankAccounts/SwedishBankAccounts.UnitTests/BankAccountNumberTests.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace SwedishBankAccounts.UnitTests;
 
-namespace SwedishBankAccounts.UnitTests
+public class BankAccountNumberTests : UnitTests
 {
-    internal class Class1
+    [Theory] 
+    [InlineData("9071, 417 23 83", "Multitude Bank")]
+    [InlineData("6683764450808", "Handelsbanken")]
+    [InlineData("8424-4,983 189 224-6", "Swedbank")]
+    public void AccountNumber_TryParse_ShouldValidate(string accountNumber, string bankName)
     {
+        BankAccountNumber.TryParse(accountNumber, out var bankAccountNumber)
+            .Should().BeTrue($"it should be validated to {bankName}");
+
+        bankAccountNumber.Should().NotBeNull($"it should be validated to {bankName}");
+        bankAccountNumber!.Bank.Should().Be(bankName, $"it should be validated to {bankName}");
     }
 }
